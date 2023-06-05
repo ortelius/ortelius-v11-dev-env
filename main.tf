81367ff265a1ac07e63f2796af63ad1cab6d3f1b
TheVestedLeopard <tvl@tvl1.local>
2023-06-05 09:16:53 +0200

    👌 IMPROVE: helm chart url fix
    Pre-Commit-Config: 6db1a456e2a9f0914f8e959159dfffd4eebbd2a22e44f8d3766359db8e235c37

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "kind_cluster" "ortelius" {
 resource "helm_release" "ortelius" {
   name              = "ortelius"
   chart             = "ortelius"
-  repository        = "https://ortelius.github.io/scec-charts/index.yaml"
+  repository        = "https://ortelius.github.io/scec-charts/index.yaml" #https://ortelius.github.io/scec-charts/index.yaml
   namespace         = var.ortelius_namespace
   create_namespace  = true
   recreate_pods     = true
